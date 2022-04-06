8a823d1492614e26ab6d5ae19f663a3c90446474
hail zanbar <hailzanbar1@gmail.com>
2022-04-06 03:22:19 +0300

    checkLogin func works when the username and password are correct, else - cant see the alert.

--- webclient/src/pages/login/Login.js
+++ webclient/src/pages/login/Login.js
@@ -1,33 +1,60 @@
 import './Login.css';
-import {Link} from "react-router-dom";
+import { useRef } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 function Login({users}) {
+
+  const usernameInput = useRef();
+  const passwordInput = useRef();
+  const navigate = useNavigate();
+
+  const checkLogin = function(){
+    let userName = usernameInput.current.value;
+    let password = passwordInput.current.value;
+
+    for (let i = 0; i < users.length; i++){
+
+      // if the username and the password are correct, move to the chats page. (working!)
+      if (userName === users[i].username && password === users[i].password){
+            navigate("/chats");
+      }
+    }
+
+    // doesnt working
     return (
-    <div id = "login">
-        <img src="logo.png" className='logo' width="20%" height="20%"></img>
+      <div class="alert alert-warning" role="alert">
+        A simple warning alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+      </div>
+    );
+  }
+
+
+    return (
+    <div id = "login"> 
         <form>
           <div class="form-group row">
             <label class="col-sm-2 col-form-label"> Username </label>
             <div class="col-sm-10">
-              <input type="text" placeholder="Enter Username" id="uname" name="uname"></input>
+              <input type="text" placeholder="Enter Username" id="uname" name="uname" ref={usernameInput}></input>
             </div>
           </div>
 
           <div class="form-group row">
             <label class="col-sm-2 col-form-label"> Password </label> 
             <div class="col-sm-10">
-              <input type="text" placeholder="Enter Password" id="password" name="password"></input>
+              <input type="text" placeholder="Enter Password" id="password" name="password" ref={passwordInput}></input>
             </div>
           </div>
           
           <div>
-            <button type="button" class="btn btn-success">Login</button>
+            <button type="button" class="btn btn-success" onClick={checkLogin}>Login</button>
           </div>
           
           <div>
-            <label> Not registred?&nbsp;</label>
+            <label> Not registred? </label>
             <a href="/regist"> Click here </a>
-            <label>&nbsp;to register </label> 
+            <label> to register </label> 
           </div>
         </form>
      </div>
